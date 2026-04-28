09bac612f575e044dc4f311c71ecc455e289a32c
delusionalbrain <167742252+delusionalbrain@users.noreply.github.com>
2026-04-28 16:26:40 +0530

    server file made

---
+++ server.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;       
+    addr.sin_port = htons(6379);     
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSocket, (sockaddr*)&addr, sizeof(addr));
+
+    listen(serverSocket, 10);
+
+    cout << "Server running on port 6379...\n";
+
+    while (true) {
+
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        cout << "Client connected!\n";
+
+        char buffer[1024] = {};
+
+        recv(clientSocket, buffer, sizeof(buffer), 0);
+
+        cout << "Received: " << buffer << "\n";
+
+        string response = "+OK\r\n";
+
+        send(clientSocket, response.c_str(), response.size(), 0);
+
+        close(clientSocket);
+    }
+    close(serverSocket);
+}
